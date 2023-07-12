8a7afca53b7c52e6d4d5a88e5bd4ac6aa2331b74
scmirele <mirelecn@gmail.com>
2023-07-12 11:43:36 -0300

    finalizando modal na página home

--- perguntas/allquestions.js
+++ perguntas/allquestions.js
@@ -1,20 +1,33 @@
-let linkModal = document.querySelector('#link-modal')
+const openModalButton = document.querySelector('#open-modal');
+const closeModalButton = document.querySelector('#close-modal');
+const fade = document.querySelector('#fade');
+const modal = document.querySelector('#modal');
 
-let changeCheck = document.querySelector('.icon-check')
-console.log(changeCheck)
+const toggleModal = () => {
+  [modal, fade].forEach((elemento) => elemento.classList.toggle("hide"));
+};
 
-let checkoutEnable = "../assets/icon-status.svg" 
-let checkoutDisable = "../assets/icon-status-disable.svg"
+[openModalButton, closeModalButton, fade].forEach((elemento) => {
+  elemento.addEventListener('click', () => toggleModal());
+});
 
-function DisableEnable() {
-  let check = document.querySelector('.icon-check')
-  check.src = checkoutEnable
-  checkoutEnable = checkoutDisable
-  checkoutDisable = check.src
-}
+// let linkModal = document.querySelector('#link-modal')
 
-changeCheck.addEventListener('click', DisableEnable)
+// let changeCheck = document.querySelector('.icon-check')
+// console.log(changeCheck)
 
-linkModal.setAttribute('href', 'http://127.0.0.1:5501/modal.html')
-linkModal.setAttribute('target', '_blank') 
+// let checkoutEnable = "../assets/icon-status.svg" 
+// let checkoutDisable = "../assets/icon-status-disable.svg"
+
+// function DisableEnable() {
+//   let check = document.querySelector('.icon-check')
+//   check.src = checkoutEnable
+//   checkoutEnable = checkoutDisable
+//   checkoutDisable = check.src
+// }
+
+// changeCheck.addEventListener('click', DisableEnable)
+
+// linkModal.setAttribute('href', 'http://127.0.0.1:5501/modal.html')
+// linkModal.setAttribute('target', '_blank') 
 
