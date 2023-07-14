24ca6049bd53622f3f97500785740605b2ec89b4
ossousa18 <osousa390@gmail.com>
2023-07-14 17:09:42 -0300

    login/js

--- login/login.js
+++ login/login.js
@@ -1,36 +1,40 @@
 let email = document.querySelector('#botaoEmail')
 let senha = document.querySelector('#botaoSenha')
 let button = document.querySelector('#botaoEntrar')
-
-fetch('https://backend-question-production.up.railway.app/usuarios').then(resposta =>{
-    return resposta.json()
-}).then((usuarios)=>{
-    listUsers(usuarios)
-}).catch((error) =>{
-    console.log('erro foi:' , error)
-})
+let form = document.querySelector('#formulario')
 
 const storedUser = JSON.parse(localStorage.getItem('info'));
-function listUsers(usuarios){
-    
-    usuarios.forEach((usuario) => {
-       const {email, senha } = usuario
-      
-        if (storedUser.botaoEmail === email && storedUser.botaoSenha === senha ){
-             
-        }else{
-            alert ('Usuário não cadastrado')
-        }
-    }); 
+
+async function loginUser(){
+    const urlUsuario = 'https://backend-question-production.up.railway.app/usuarios/login'
+
+    let dados = {
+        email: email.value,
+        senha: senha.value
+    }
+
+    try {
+        const responseLogin = await fetch(urlUsuario, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(dados)
+        })
+
+        const dataLogin = await responseLogin.json()
+        console.log(dataLogin)
+        
+    } catch (error) {
+        alert('Usuário não encontrado')
+    }
 }
 
-button.addEventListener('click',()=>{
-    const info ={
-        botaoEmail:email.value,
-        botaoSenha:senha.value
-    }
-localStorage.setItem('info',JSON.stringify(info))
-})
+form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    loginUser()
 
+     
 
-
+ })
+ 
